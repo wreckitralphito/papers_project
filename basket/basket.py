848bf3ce3ae194213a5024a7b7342529ae176a47
dygi-slay <therese.dy@obf.ateneo.edu>
2023-07-20 01:09:50 +0800

    erica 3:57:50

--- basket/basket.py
+++ basket/basket.py
@@ -19,10 +19,12 @@ class Basket():
         """
         product_id = str(product.id)
 
-        if product_id not in self.basket:
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
             self.basket[product_id] = {'price': str(product.price), 'qty':int(qty)}
 
-        self.session.modified = True
+        self.save()
     
     def __iter__(self):
         """
@@ -47,3 +49,27 @@ class Basket():
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
 
+    def delete(self, product):
+        """
+        Delete item from session data
+        """
+        product_id = str(product)
+
+        if product_id in self.basket:
+            del self.basket[product_id]
+            self.save()
+
+    def update(self, product, qty):
+        """
+        Update values in session data
+        """
+        product_id = product
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+
+        self.save()
+
+
+    def save(self):
+        self.session.modified = True
