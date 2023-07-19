04bdb1d17cd4fef58d39d299dec390684e038831
wreckitralphito <ralph.relucio@obf.ateneo.edu>
2023-07-19 18:16:57 +0800

    ralph 3:13:00

--- basket/basket.py
+++ basket/basket.py
@@ -1,5 +1,5 @@
-
-
+from decimal import Decimal
+from store.models import Product
 
 class Basket():
     """
@@ -23,3 +23,27 @@ class Basket():
             self.basket[product_id] = {'price': str(product.price), 'qty':int(qty)}
 
         self.session.modified = True
+    
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the database
+        and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+        
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+    def __len__(self):
+        return sum(item['qty'] for item in self.basket.values())
+    
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
